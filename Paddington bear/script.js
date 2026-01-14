2bb254c779d3e04285fbc7433f9a4b7bd773cecd
goodloosey <caleb.costa.god@gmail.com>
2026-01-14 11:30:17 -0300

    botei o clique que faz o botao sumir.

--- Paddington bear/script.js
+++ Paddington bear/script.js
@@ -1,6 +1,9 @@
+let botao = document.getElementById("modo")
 let corpo = document.body
 let mudarModo = () => {
     corpo.style.backgroundColor = 'black'
     corpo.style.color = 'white'
+    botao.style.display = 'none'
+    
     
 }
